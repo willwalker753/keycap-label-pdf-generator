0bd492aadcd1cbca14ccee93256e2b2afefd42f3
Will Walker <willwalkerdev@gmail.com>
2025-07-23 13:03:17 -0500

    adjust label sizes

--- generateLatex.py
+++ generateLatex.py
@@ -1,14 +1,19 @@
 import os
 
 # Configuration
-CELL_HEIGHT = "1.3cm"
-CELL_WIDTH = "1.2cm"
+CELL_HEIGHT = "1.32cm"
+CELL_WIDTH = "1.19cm"
+CELL_INNER_PADDING = "2pt"
 COLUMN_COUNT = 15 # Number of columns per row (adjust based on page width)
 FONT_FAMILY = "phv" # Font family: e.g. cmr (Computer Modern), lmr (Latin Modern), ptm (Times), phv (Helvetica)
 FONT_SIZE = 7
 FONT_LINE_SPACING = 9
 INPUT_FILE = "input_labels.txt"  # Input file with switch names (one per line)
 OUTPUT_FILE = "output_labels.tex"
+
+assert CELL_INNER_PADDING[-2:] in ['pt', 'cm'], f'Unexpected CELL_INNER_PADDING measurement unit. Value: {CELL_INNER_PADDING}'
+_CELL_INNER_PADDING_DOUBLED_FLOAT = float(CELL_INNER_PADDING[:-2]) * 2
+_CELL_INNER_PADDING_DOUBLED = str(_CELL_INNER_PADDING_DOUBLED_FLOAT if _CELL_INNER_PADDING_DOUBLED_FLOAT % 1 > 0 else int(_CELL_INNER_PADDING_DOUBLED_FLOAT)) + CELL_INNER_PADDING[-2:]
 
 # Read switch names from file
 def read_switch_names(file_path):
@@ -47,18 +52,27 @@ def generate_latex(names):
 \usepackage[table]{xcolor}
 \usepackage{arydshln}
 
-\definecolor{lightgray}{RGB}{180,180,180} % Light gray color
+\definecolor{lightgray}{RGB}{140,140,140} % Light gray color
 \arrayrulecolor{lightgray}
 
 \setlength{\dashlinedash}{2pt}
 \setlength{\dashlinegap}{4pt}
 
+% centerinig column type with horizontal padding
 \newcolumntype{C}[1]{%
-  >{\begin{minipage}[c][''' + CELL_HEIGHT + r'''][c]{#1}\centering\arraybackslash}% 
-  p{#1}% 
-  <{\end{minipage}}%
+  >{\begin{minipage}[c][1.32cm][c]{#1}% Outer minipage (fixed dimensions)
+    \hspace*{''' + CELL_INNER_PADDING + r'''}% Left padding
+    \begin{minipage}[c]{\dimexpr#1-''' + _CELL_INNER_PADDING_DOUBLED + r'''}% Inner minipage (adjusted width)
+    \centering\arraybackslash
+  }%
+  p{#1}% Column width specification
+  <{%
+    \end{minipage}% Close inner minipage
+    \hspace*{''' + CELL_INNER_PADDING + r'''}% Right padding
+  \end{minipage}}% Close outer minipage
 }
-\setlength{\tabcolsep}{0pt} 
+\setlength{\tabcolsep}{0pt} % Remove inter-column padding
+
 
 \begin{document}
 \pagestyle{empty}
